b3f8fb74bed21ed21118cfd964db547bee510c45
Mark Lopez <m@silvenga.com>
2017-09-22 20:18:42 -0500

    Improved log output.

--- files/init.sh
+++ files/init.sh
@@ -3,11 +3,11 @@
 CONFIG_SCRIPT="/config.sh"
 
 if [ -f $CONFIG_SCRIPT ]; then
-    echo "$CONFIG_SCRIPT found, will run."
+    echo "Config script $CONFIG_SCRIPT found, will run."
     /bin/bash $CONFIG_SCRIPT
     echo "Script executed and exited with code $?."
 else
-    echo "$CONFIG_SCRIPT not found, will not run."
+    echo "Config script $CONFIG_SCRIPT not found, will not run."
 fi
 
 echo "Starting Supervisor."
